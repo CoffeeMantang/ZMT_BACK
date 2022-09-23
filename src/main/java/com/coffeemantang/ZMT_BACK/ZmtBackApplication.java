bf9d8314ec5c32ebccf1dce85fb512e6cff3dae0
Lv2dev <lv2dev@gmail.com>
2022-09-23 12:19:49 +0900

    first commit

---
+++ src/main/java/com/coffeemantang/ZMT_BACK/ZmtBackApplication.java
@@ -0,0 +1,13 @@
+package com.coffeemantang.ZMT_BACK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmtBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZmtBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeemantang/ZMT_BACK/ZmtBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeemantang.ZMT_BACK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZmtBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
