e743ceb9af5656958f8bb5a909c54b97b81bcaff
Lv2dev <lv2dev@gmail.com>
2022-10-28 12:29:51 +0900

    import 오류 해결

--- src/main/java/com/coffeemantang/ZMT_BACK/controller/ReviewController.java
+++ src/main/java/com/coffeemantang/ZMT_BACK/controller/ReviewController.java
@@ -7,13 +7,13 @@ import com.coffeemantang.ZMT_BACK.persistence.ReviewCommentRepository;
 import com.coffeemantang.ZMT_BACK.service.ReviewService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.ws.Response;
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Slf4j
